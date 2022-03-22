597e4ef86deaf9f3adb24f8d1b0b9162c56ca0a6
JordiEspinozaMendoza <jordi8101@gmail.com>
2022-03-21 18:33:12 -0700

    Updated readme

---
+++ problem_29.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+// Problem 28
+// Se juega una mano de póquer. Encontrar la probabilidad de que
+// la mano contenga al menos una pareja, suponiendo que no contiene
+// ases, dieces o cartas de cara (rey, reina, caballero)
+
+// @Author @JordiEspinozaMendoza
+// @Date 21 March 2022
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    int times = 0;
+    int totalGames = 1000000;
+
+}
